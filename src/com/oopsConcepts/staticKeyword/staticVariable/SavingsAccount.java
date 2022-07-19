bc257ec5caa5ebf91cf58b9a8d1ff53e8a0acf3b
karthik <karthik17ec031@bnmit.in>
2022-07-19 10:31:31 +0530

    commit

--- src/com/oopsConcepts/staticKeyword/staticVariable/SavingsAccount.java
+++ src/com/oopsConcepts/staticKeyword/staticVariable/SavingsAccount.java
@@ -54,5 +54,18 @@ public class SavingsAccount {
 			
 		}
 		
+		public static void main(String[] args) 
+		{
+			// 1 savings account details  ( 1, laila , 2000 ) 
+			SavingsAccount.setAccountDetails( 1 , "laila" , 2000 ) ;
+			SavingsAccount.displayAccountDetails()  ; 
+			// i need to deposit 500 
+			SavingsAccount.deposit( 500  )  ; 
+			SavingsAccount.displayAccountDetails()  ; 
+			
+			SavingsAccount.withdrawn( 2500  )  ; 
+			SavingsAccount.displayAccountDetails()  ; 
+		}
+
 	
 }
--- src/com/oopsConcepts/staticKeyword/staticVariable/SavingsAccountDriver.java
+++ src/com/oopsConcepts/staticKeyword/staticVariable/SavingsAccountDriver.java
@@ -2,17 +2,17 @@ package com.oopsConcepts.staticKeyword.staticVariable;
 
 public class SavingsAccountDriver {
     
-	public static void main(String[] args) 
-	{
-		// 1 savings account details  ( 1, laila , 2000 ) 
-		SavingsAccount.setAccountDetails( 1 , "laila" , 2000 ) ;
-		SavingsAccount.displayAccountDetails()  ; 
-		// i need to deposit 500 
-		SavingsAccount.deposit( 500  )  ; 
-		SavingsAccount.displayAccountDetails()  ; 
-		
-		SavingsAccount.withdrawn( 2500  )  ; 
-		SavingsAccount.displayAccountDetails()  ; 
-	}
+//	public static void main(String[] args) 
+//	{
+//		// 1 savings account details  ( 1, laila , 2000 ) 
+//		SavingsAccount.setAccountDetails( 1 , "laila" , 2000 ) ;
+//		SavingsAccount.displayAccountDetails()  ; 
+//		// i need to deposit 500 
+//		SavingsAccount.deposit( 500  )  ; 
+//		SavingsAccount.displayAccountDetails()  ; 
+//		
+//		SavingsAccount.withdrawn( 2500  )  ; 
+//		SavingsAccount.displayAccountDetails()  ; 
+//	}
 	
 }
