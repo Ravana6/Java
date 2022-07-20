3508b4ca36b97639977fbb521f4457ee1ab8e04a
karthik <karthik17ec031@bnmit.in>
2022-07-20 10:38:34 +0530

    Day1

---
+++ src/com/day1/introduction/FirstProgram.java
@@ -0,0 +1,8 @@
+package com.day1.introduction;
+
+public class FirstProgram {
+          public static void main(String[] args) 
+          {
+	              System.out.println("\" karthik s\""); 
+           }
+}
