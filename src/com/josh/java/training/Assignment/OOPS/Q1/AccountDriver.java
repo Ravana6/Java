bbe7e9a6c0f4a419d122e1972847e7ca9f8115a9
karthik s <karthik17ec031@bnmit.in>
2022-07-27 10:49:42 +0530

    small modify

--- src/com/josh/java/training/Assignment/OOPS/Q1/AccountDriver.java
+++ src/com/josh/java/training/Assignment/OOPS/Q1/AccountDriver.java
@@ -3,13 +3,13 @@ package com.josh.java.training.Assignment.OOPS.Q1;
 public class AccountDriver {
 
 	public static void main(String[] args) {
-		Account account_Holder_1=new Account("06","karthik" );  //creating account holder 1
+		Account accountHolder1=new Account("06","karthik" );  //creating account holder 1
 		
-		System.out.println("checking balanceaccount holder 1=  "+account_Holder_1.getBalance());
-		account_Holder_1.credit(6000);
-		account_Holder_1.debit(7000);
-		account_Holder_1.debit(5000);
-		System.out.println(account_Holder_1.toString());
+		System.out.println("checking balanceaccount holder 1=  "+accountHolder1.getBalance());
+		accountHolder1.credit(6000);
+		accountHolder1.debit(7000);
+		accountHolder1.debit(5000);
+		System.out.println(accountHolder1);
 		
 		System.out.println("===================");
 		
@@ -25,11 +25,11 @@ public class AccountDriver {
 		
 		
 	   //checking balance of account holder 1
-		System.out.println("checking balance of account holder 1 ="+account_Holder_1.getBalance());
+		System.out.println("checking balance of account holder 1 ="+accountHolder1.getBalance());
 		
 
 		  //transferring  amount  from account holder 1 to account holder 2
-		account_Holder_1.transferTo(account_Holder_2, 500);
+		accountHolder1.transferTo(account_Holder_2, 500);
 		
 		
 		
